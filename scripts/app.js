172d9db5b952688ca58f1f6bfe710ee42028e457
Chris Kennedy <ckennedy0323@gmail.com>
2016-11-21 18:54:29 -0500

    General Structure and Index

---
+++ scripts/app.js
@@ -0,0 +1,16 @@
+// fork getUserMedia for multiple browser versions, for those
+// that need prefixes
+
+navigator.getUserMedia = (navigator.getUserMedia ||
+                          navigator.webkitGetUserMedia ||
+                          navigator.mozGetUserMedia ||
+                          navigator.msGetUserMedia);
+
+// set up forked web audio context, for multiple browsers
+// window. is needed otherwise Safari explodes
+
+var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+var voiceSelect = document.getElementById("voice");
+var source;
+var stream;
+
