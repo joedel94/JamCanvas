d124008625bf265337d51c0af5b43dd7f101bbf8
Betina Dalope <mbetinad@gmail.com>
2016-11-25 15:40:45 -0500

    front end structure

---
+++ scripts/control.js
@@ -0,0 +1,12 @@
+var currentPage = 0;
+var pages = {
+	0: "startPage",
+	1: "selectPage",
+	2: "canvasPage"
+}
+$(document).ready(function() {
+	for (var i in pages) {
+		var pageName = pages[i];
+		$("#" + pageName).load("scripts/" + pageName + ".html");
+	}
+});
