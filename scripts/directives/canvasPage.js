5229559ce86430a1021d7b0764d9c34570cadf2e
Betina Dalope <mbetinad@gmail.com>
2017-02-06 22:15:21 -0500

    calculate windows

--- scripts/directives/canvasPage.js
+++ scripts/directives/canvasPage.js
@@ -1,167 +1,33 @@
 console.log("canvasPage");
 // that need prefixes
 
-navigator.getUserMedia = (navigator.getUserMedia ||
-                          navigator.webkitGetUserMedia ||
-                          navigator.mozGetUserMedia ||
-                          navigator.msGetUserMedia);
+calculateWindows();
 
-// set up forked web audio context, for multiple browsers
-// window. is needed otherwise Safari explodes
+//calculate before each building is added and remove .each() function
+// and do $(".windows", element).each();
+function calculateWindows(element) {
 
-var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
-var voiceSelect = document.getElementById("voice");
-var source;
-var stream;
+  var border_size = parseInt($("#window-model").css("border-left-width"));
+  var window_height = $("#window-model").height() + (2 * border_size);
+  var window_width = $("#window-model").width() + (2 * border_size);
+  $(".windows").each(function() {
 
-var canvas = document.querySelector('.visualizer');
-var canvasCtx = canvas.getContext("2d");
 
-var analyser = audioCtx.createAnalyser();
-analyser.minDecibels = -90;
-analyser.maxDecibels = -10;
-analyser.smoothingTimeConstant = 0.85;
+    var windows_per_row = Math.floor($(this).width() / window_width);
+    var num_window_rows = Math.floor($(this).height() / window_height);
 
-// var intendedWidth = document.querySelector('.wrapper').clientWidth;
+    console.log(windows_per_row, num_window_rows);
 
-// canvas.setAttribute('width',intendedWidth);
-
-var visualSelect = document.getElementById("visual");
-var waveShaper = audioCtx.createWaveShaper();
-
-var drawVisual;
-
-//main block for doing the audio recording
-
-if (navigator.getUserMedia) {
-   console.log('getUserMedia supported.');
-   navigator.getUserMedia (
-      // constraints - only audio needed for this app
-      {
-         audio: true
-      },
-
-      // Success callback
-      function(stream) {
-         source = audioCtx.createMediaStreamSource(stream);
-         source.connect(analyser);
-         analyser.connect(waveShaper);
-
-      	 visualize();
-      },
-
-      // Error callback
-      function(err) {
-         console.log('The following gUM error occured: ' + err);
+    var windows_html = "";
+    while (num_window_rows > 0) {
+      windows_html += "<div class='window-row'>"
+      for (var i = 0; i < windows_per_row; i++) {
+        windows_html += "<div class='window'></div>"
       }
-   );
-} else {
-   console.log('getUserMedia not supported on your browser!');
-}
+      windows_html +="</div>"; 
+      num_window_rows--;
+    }
 
-function visualize() {
-  WIDTH = canvas.width;
-  HEIGHT = canvas.height;
-
-
-  var visualSetting = visualSelect.value;
-
-  if(visualSetting == "sinewave") {
-    analyser.fftSize = 2048;
-    var bufferLength = analyser.fftSize;
-    var freqBuffLength = analyser.frequencyBinCount;
-    var freqDataArray = new Uint8Array(freqBuffLength);
-    var dataArray = new Uint8Array(bufferLength);
-
-    canvasCtx.clearRect(0, 0, WIDTH, HEIGHT);
-
-    function draw() {
-
-      drawVisual = requestAnimationFrame(draw);
-
-      analyser.getByteTimeDomainData(dataArray);
-      analyser.getByteFrequencyData(freqDataArray);
-
-
-      canvasCtx.fillStyle = 'rgb(200, 200, 200)';
-      canvasCtx.fillRect(0, 0, WIDTH, HEIGHT);
-
-      canvasCtx.lineWidth = 2;
-      canvasCtx.strokeStyle = 'rgb(0, 0, 0)';
-
-      canvasCtx.beginPath();
-
-      var sliceWidth = WIDTH * 1.0 / bufferLength;
-      var x = 0;
-
-      for(var i = 0; i < bufferLength; i++) {
-   
-        var v = dataArray[i] / 128.0;
-        var y = v * HEIGHT/2;
-
-        if(freqDataArray[i]>140){
-          canvasCtx.strokeStyle = 'rgb(' + (freqDataArray[i]+50) + ',100,100)';
-          if(freqDataArray[i]>150){
-		      canvasCtx.strokeStyle = 'rgb(' + (freqDataArray[i]+100) + ',50,50)'; 
-          }
-        }
-
-        if(i === 0) {
-          canvasCtx.moveTo(x, y);
-        } else {
-          canvasCtx.lineTo(x, y);
-        }
-
-        x += sliceWidth;
-      }
-
-      canvasCtx.lineTo(canvas.width, canvas.height/2);
-      canvasCtx.stroke();
-    };
-
-    draw();
-
-  } else if(visualSetting == "frequencybars") {
-    analyser.fftSize = 256;
-    var bufferLength = analyser.frequencyBinCount;
-    console.log(bufferLength);
-    var dataArray = new Uint8Array(bufferLength);
-
-    canvasCtx.clearRect(0, 0, WIDTH, HEIGHT);
-
-    function draw() {
-      drawVisual = requestAnimationFrame(draw);
-
-      analyser.getByteFrequencyData(dataArray);
-
-      canvasCtx.fillStyle = 'rgb(200, 200, 200)';
-      canvasCtx.fillRect(0, 0, WIDTH, HEIGHT);
-
-      var barWidth = (WIDTH / bufferLength) * 2.5;
-      var barHeight;
-      var x = 0;
-
-      for(var i = 0; i < bufferLength; i++) {
-        barHeight = dataArray[i];
-
-        canvasCtx.fillStyle = 'rgb(' + (barHeight+100) + ',50,50)';
-        canvasCtx.fillRect(x,HEIGHT-barHeight/2,barWidth,barHeight/2);
-
-        x += barWidth + 1;
-      }
-    };
-
-    draw();
-
-  } else if(visualSetting == "off") {
-    canvasCtx.clearRect(0, 0, WIDTH, HEIGHT);
-    canvasCtx.fillStyle = "red";
-    canvasCtx.fillRect(0, 0, WIDTH, HEIGHT);
-  }
-
-}
-
-visualSelect.onchange = function() {
-  window.cancelAnimationFrame(drawVisual);
-  visualize();
+    $(this).html(windows_html);
+  });
 }
