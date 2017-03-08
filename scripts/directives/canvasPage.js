cfc07721aab80898f7a4429879f5135b22f264e0
Betina Dalope <mbetinad@gmail.com>
2017-03-07 20:14:37 -0500

    demolish function

--- scripts/directives/canvasPage.js
+++ scripts/directives/canvasPage.js
@@ -4,66 +4,76 @@ console.log("canvasPage");
 // and do $(".windows", element).each();
 function calculateWindows(element) {
 
-  var border_size = parseInt($("#window-model").css("border-left-width"));
-  var window_height = $("#window-model").height() + (2 * border_size);
-  var window_width = $("#window-model").width() + (2 * border_size);
-  $(".windows").each(function() {
+	var border_size = parseInt($("#window-model").css("border-left-width"));
+	var window_height = $("#window-model").height() + (2 * border_size);
+	var window_width = $("#window-model").width() + (2 * border_size);
+	$(".windows").each(function() {
 
 
-    var windows_per_row = Math.floor($(this).width() / window_width);
-    var num_window_rows = Math.floor($(this).height() / window_height);
+		var windows_per_row = Math.floor($(this).width() / window_width);
+		var num_window_rows = Math.floor($(this).height() / window_height);
 
-    console.log(windows_per_row, num_window_rows);
+		console.log(windows_per_row, num_window_rows);
 
-    var windows_html = "";
-    while (num_window_rows > 0) {
-      windows_html += "<div class='window-row'>"
-      for (var i = 0; i < windows_per_row; i++) {
-        windows_html += "<div class='window'></div>"
-      }
-      windows_html +="</div>"; 
-      num_window_rows--;
-    }
+		var windows_html = "";
+		while (num_window_rows > 0) {
+			windows_html += "<div class='window-row'>"
+			for (var i = 0; i < windows_per_row; i++) {
+				windows_html += "<div class='window'></div>"
+			}
+			windows_html +="</div>"; 
+			num_window_rows--;
+		}
 
-    $(this).html(windows_html);
-  });
+		$(this).html(windows_html);
+	});
 }
 
 
 var buildings = [
-  '<div class="building building0"><div class="shape block4"></div><div class="block block3"></div><div class="block block2"></div><div class="block block1 windows"></div><div class="block block0"></div></div>',
-  '<div class="building building1"><div class="block block3"></div><div class="block block2"></div><div class="block block1 windows"></div><div class="block block0"></div></div>'
+	'<div class="building building0"><div class="shape block4"></div><div class="block block3"></div><div class="block block2"></div><div class="block block1 windows"></div><div class="block block0"></div></div>',
+	'<div class="building building1"><div class="block block3"></div><div class="block block2"></div><div class="block block1 windows"></div><div class="block block0"></div></div>'
 ]
 
 var renderAnimation = {
-  maxLowFrequency: 100, //dictate 100% height for each level
-  maxMedFrequency: 200,
-  maxHighFrequency: 300,
-  buildings: [],
+	maxLowFrequency: 100, //dictate 100% height for each level
+	maxMedFrequency: 200,
+	maxHighFrequency: 300,
+	buildings: [],
 
-  init: function() {
+	init: function() {
 
-  },
-  selectBuilding: function() {
-    var min = 0;
-    var max = buildings.length - 1;
-    var rand = Math.floor(Math.random() * (max - min + 1)) + min;
-    return $($.parseHTML(buildings[rand])); //maybe have preparsed buidings in the array
-    
-  },
-  endConstruction: function(element) {
-    $(element).removeClass("in-construction");
-  },
-  // demolishBuilding: function(element) {
-  //   $(element).remove();
-  // },
-  renderLiveBuilding: function(frequency) {
-    var scaffolding = this.selectBuilding().addClass('high-frequency in-construction');
-    $("#liveNoiseFactory").append(scaffolding);
+	},
+	selectBuilding: function() {
+		var min = 0;
+		var max = buildings.length - 1;
+		var rand = Math.floor(Math.random() * (max - min + 1)) + min;
+		return $($.parseHTML(buildings[rand])); //maybe have preparsed buidings in the array
+		
+	},
+	endConstruction: function(element) {
+		$(element).removeClass("in-construction");
+		$(element).addClass("animating")
+	},
+	demolishBuilding: function(element) {
+		$(element).remove();
+	},
+	renderLiveBuilding: function(frequency) {
+		var scaffolding = this.selectBuilding().addClass('high-frequency in-construction');
+		$("#liveNoiseFactory").append(scaffolding);
 
-    scaffolding = $("#liveNoiseFactory .in-construction");
-    TweenMax.set(scaffolding, {right: 0});
-    TweenMax.to(scaffolding, 2, {right: '100%', onStart: renderAnimation.endConstruction(scaffolding)});
-  }
+		scaffolding = $("#liveNoiseFactory .in-construction");
+		var finishedBuilding = $("#liveNoiseFactory .animating");
+
+		var t = 5; //calculate using BPM
+		TweenMax.set(scaffolding, {right: 0});
+		TweenMax.to(scaffolding, t, {
+			right: '100%',
+			onStart: this.endConstruction(scaffolding),
+			ease: Power0.easeNone
+		});
+
+		TweenMax.delayedCall(t, function() { renderAnimation.demolishBuilding(finishedBuilding)});
+	}
 }
 
