7ac5cf29bdabfb58357b557eff07014d5546cddb
shezwick <cet72@drexel.edu>
2017-05-07 22:13:32 -0400

    added new node library (azure-storage) and attempted to turn the python into javascript no idea how to integrate it though, use the upload.js file as template and the upload.py file for clarification of functions if needed

---
+++ scripts/upload.js
@@ -0,0 +1,39 @@
+var azure = require('azure-storage');
+var fs = require('fs');
+
+var blobService = azure.createBlobService();
+
+//will need some way to pass the container name into 
+blobService.createContainerIfNotExists('containerName', {publicAccessLevel : 'container'}, function(error, result, response){
+	if(!error){
+		//Do something
+	}
+});
+
+//same as above, with addition of the blob name and file path
+//can use file stream if unable to directly access client files
+blobService.createBlockBlobFromLocalFile('containerName', 'blobName', 'filePath', function(error, result, response){
+	if(!error){
+		//Do something
+	}
+});
+
+//result.entries contains any blob names found
+blobService.listBlobsSegmented('containerName', null, function(error, result, response){
+	if(!error){
+		//Do something
+	}
+});
+
+//will grab a blob content and store it locally
+blobService.getBlobToStream('containerName', 'blobName', fs.createWriteStream('outputPath'), function(error, result, response){
+	if(!error){
+		//Do something
+	}
+});
+
+blobService.deleteContainerIfExists('containerName', function(error, result, response){
+	if(!error){
+		//Do something
+	}
+});
