8c40e29339c7c6e28da1ac37ab5b7a3e866f4b07
Betina Dalope <mrd88@drexel.edu>
2017-05-15 13:22:20 -0400

    select upload vs. demo

--- scripts/directives/selectPage.js
+++ scripts/directives/selectPage.js
@@ -1,5 +1,12 @@
 console.log("selectPage");
 
+function selectUploadOption() {
+	$(".begin-projection").removeClass("disabled");
+}
+
 function selectDemo() {
 	demoSong = $("#demo").val();
+	if (demoSong) {
+		selectUploadOption();
+	}	
 }
