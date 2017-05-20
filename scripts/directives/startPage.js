9a62fea171c412db09f8665e227689d7abc94a3a
Betina Dalope <mrd88@drexel.edu>
2017-05-20 14:18:00 -0400

    staff animation so far

--- scripts/directives/startPage.js
+++ scripts/directives/startPage.js
@@ -1,1 +1,27 @@
-console.log("startPage");
+$("#startPage").ready(function(event) {
+    attractAnimation.init();
+});
+
+var attractAnimation = {
+	staffLines: null,
+	init: function() {
+		this.staffLines = $(".staff .line");
+
+		this.setElements();
+		this.aniStaffLines();
+		this.aniNotes();
+	},
+	setElements: function() {
+		TweenMax.set(this.staffLines, {width: "0%"});
+	},
+	aniStaffLines: function() {
+		var d = 0;
+		
+		$(this.staffLines).each(function() {
+			TweenMax.to(this, .5, {width: "100%", delay: d});
+			d += .3;
+		});
+	},
+	aniNotes: function() {
+	}
+}
